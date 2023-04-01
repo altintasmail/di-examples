e4f827e44bc29c6d18d41930633058ac2f9682d8
Abdurrahman <altintasmail@gmail.com>
2023-04-01 13:32:55 +0300

    httpcontext current items

---
+++ WebFormDIExample/HttpContextCurrentItems/ServiceLocator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using WebApplication2.HttpModules;
+
+namespace WebApplication2.Helpers
+{
+    public class ServiceLocator : IServiceProvider
+    {
+        public static T GetService<T>()
+        {
+            var scope = (IServiceScope)HttpContext.Current.Items[typeof(IServiceScope)];
+            return scope.ServiceProvider.GetService<T>();
+        }
+
+        public static T GetRequiredService<T>()
+        {
+            var scope = (IServiceScope)HttpContext.Current.Items[typeof(IServiceScope)];
+            return scope.ServiceProvider.GetRequiredService<T>();
+        }
+        public object GetService(Type serviceType)
+        {
+            //Debug.WriteLine(serviceType.Name);
+            var scope = (IServiceScope)HttpContext.Current?.Items[typeof(IServiceScope)];
+            try
+            {
+                //önce service provider veya public create instance denenir
+                return ActivatorUtilities.GetServiceOrCreateInstance(scope == null ? Global.ServiceProvider : scope.ServiceProvider, serviceType);
+            }
+            catch (InvalidOperationException)
+            {
+                //yoksa herhangi bir constructor ile oluşturma denenir.
+                return Activator.CreateInstance(serviceType, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.CreateInstance, null, null, null);
+            }
+        }
+    }
+}
---
+++ WebFormDIExample/HttpContextCurrentItems/ServiceLocatorHttpModule.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.HttpModules
+{
+    public class ServiceLocatorHttpModule : IHttpModule
+    {
+        //public IServiceScope Scope { get; internal set; }
+        public void Dispose()
+        {
+            //Scope.Dispose();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            //Scope = Global.ServiceProvider.CreateScope();
+            context.BeginRequest += new System.EventHandler(Begin);
+            context.EndRequest += new System.EventHandler(End);
+        }
+
+        private void Begin(Object Sender, EventArgs e)
+        {
+            HttpContext.Current.Items[typeof(IServiceScope)] = Global.ServiceProvider.CreateScope();
+        }
+
+        private void End(Object Sender, EventArgs e)
+        {
+            var scope = (IServiceScope)HttpContext.Current.Items[typeof(IServiceScope)];
+            scope.Dispose();
+        }
+    }
+}
