bfe96d6a63e3c8e179ddc3bd54ced051e7600212
Abdurrahman <altintasmail@gmail.com>
2023-04-01 16:34:28 +0300

    log servisi de ekledim

---
+++ WebFormDIExample/ILogService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+
+namespace Services
+{
+    public interface ILogService
+    {
+        void Log(string message);
+    }
+
+    public class LogService : ILogService,IDisposable
+    {
+        public Guid id { get; set; }
+        public LogService()
+        {
+            id=Guid.NewGuid();
+            Debug.WriteLine($"{id} Created");
+        }
+        public void Log(string message)
+        {
+            Debug.WriteLine($"{message} from {id}");
+        }
+
+        public void Dispose()
+        {
+            Debug.WriteLine($"{id} Disposing");
+        }
+    }
+}
