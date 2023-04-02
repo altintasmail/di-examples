26bd49830f880f25d4e16df94279013a5b499b80
Abdurrahman <altintasmail@gmail.com>
2023-04-02 14:26:47 +0300

    Web Form DI change module to Context Request

--- WebFormDIExample/ServiceLocator.cs
+++ WebFormDIExample/ServiceLocator.cs
@@ -14,29 +14,58 @@ namespace WebApplication2.Helpers
     {
         public static T GetService<T>()
         {
-            var locator = (ServiceLocatorHttpModule)HttpContext.Current.ApplicationInstance.Modules[nameof(ServiceLocatorHttpModule)];
-            return locator.Scope.ServiceProvider.GetService<T>();
+            var scope = GetLifeTimeScop();
+            return scope.ServiceProvider.GetService<T>();
         }
 
         public static T GetRequiredService<T>()
         {
-            var locator = (ServiceLocatorHttpModule)HttpContext.Current.ApplicationInstance.Modules[nameof(ServiceLocatorHttpModule)];
-            return locator.Scope.ServiceProvider.GetRequiredService<T>();
+            var scope = GetLifeTimeScop();
+            return scope.ServiceProvider.GetRequiredService<T>();
         }
         public object GetService(Type serviceType)
         {
-            //Debug.WriteLine(serviceType.Name);
-            var locator = (ServiceLocatorHttpModule)HttpContext.Current?.ApplicationInstance?.Modules[nameof(ServiceLocatorHttpModule)];
             try
             {
+                IServiceScope lifetimeScope = GetLifeTimeScop();         
                 //önce service provider veya public create instance denenir
-                return ActivatorUtilities.GetServiceOrCreateInstance(locator == null ? Global.ServiceProvider : locator.Scope.ServiceProvider, serviceType);
+                return ActivatorUtilities.GetServiceOrCreateInstance(lifetimeScope == null ? Global.ServiceProvider : lifetimeScope.ServiceProvider, serviceType);
             }
             catch (InvalidOperationException)
             {
                 //yoksa herhangi bir constructor ile oluşturma denenir.
                 return Activator.CreateInstance(serviceType, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.CreateInstance, null, null, null);
             }
+        }
+
+        private static IServiceScope GetLifeTimeScop()
+        {
+            var currentHttpContext = HttpContext.Current;
+            if (currentHttpContext != null)
+            {
+                var lifetimeScope = (IServiceScope)currentHttpContext.Items[typeof(IServiceScope)];
+                if (lifetimeScope == null)
+                {
+                    void CleanScope(object sender, EventArgs args)
+                    {
+                        if (sender is HttpApplication application)
+                        {
+                            application.RequestCompleted -= CleanScope;
+                            lifetimeScope.Dispose();
+                            Debug.WriteLine("Clean");
+                        }
+                    }
+
+                    lifetimeScope = Global.ServiceProvider.CreateScope();
+                    currentHttpContext.Items.Add(typeof(IServiceScope), lifetimeScope);
+                    currentHttpContext.ApplicationInstance.RequestCompleted += CleanScope;
+                    Debug.WriteLine("Create");
+                }
+
+                return lifetimeScope;
+            }
+
+            return null;
         }
     }
 }
--- WebFormDIExample/ServiceLocatorHttpModule.cs
+++ WebFormDIExample/ServiceLocatorHttpModule.cs
@@ -6,22 +6,30 @@ using System.Web;
 
 namespace WebApplication2.HttpModules
 {
-    public class ServiceLocatorHttpModule : IHttpModule
-    {
-        public IServiceScope Scope { get; internal set; }
-        public void Dispose()
-        {
-            Scope.Dispose();
-        }
+    // public class ServiceLocatorHttpModule : IHttpModule
+    // {
+    //     //public IServiceScope Scope { get; internal set; }
+    //     public void Dispose()
+    //     {
+    //         //Scope.Dispose();
+    //     }
 
-        public void Init(HttpApplication context)
-        {
-            Scope = Global.ServiceProvider.CreateScope();
-            //context.BeginRequest += new System.EventHandler(Begin);           
-        }
+    //     public void Init(HttpApplication context)
+    //     {
+    //         //Scope = Global.ServiceProvider.CreateScope();
+    //         context.BeginRequest += new System.EventHandler(Begin);
+    //         context.EndRequest += new System.EventHandler(End);
+    //     }
 
-        //private void Begin(Object Sender, EventArgs e)
-        //{
-        //}
-    }
+    //     private void Begin(Object Sender, EventArgs e)
+    //     {
+    //         HttpContext.Current.Items[typeof(IServiceScope)] = Global.ServiceProvider.CreateScope();
+    //     }
+
+    //     private void End(Object Sender, EventArgs e)
+    //     {
+    //         var scope = (IServiceScope)HttpContext.Current.Items[typeof(IServiceScope)];
+    //         scope.Dispose();
+    //     }
+    // }
 }
